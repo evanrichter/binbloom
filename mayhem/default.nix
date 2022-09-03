5f243440bc13b95dbbef5518df0af2e06b9555bb
Evan Richter <evanjrichter@gmail.com>
2022-09-03 13:51:24 -0500

    mayhem integration

--- mayhem/default.nix
+++ mayhem/default.nix
@@ -8,18 +8,12 @@ let
   });
 
   buildImage = pkgs.dockerTools.buildImage;
-  pullImage = pkgs.dockerTools.pullImage;
 in
-rec {
-  #debian = pullImage {
-  #  imageName = "library/debian";
-  #  sha256 = "0000000000000000000000000000000000000000000000000000";
-  #  imageDigest = "latest";
-  #};
 
+rec {
   build = buildImage {
     name = "binbloom";
-    #fromImage = debian;
+    tag = "latest";
     contents = [ binbloom ];
   };
 }
